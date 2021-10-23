9a4bcaa70c1403bd65791f5a6151908152672a5c
Grace Liu <graceliu@Graces-Air.home>
2021-10-22 23:11:41 -0400

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,42 @@
 //ask password criteria when user clicks"generate password button
-document.getElementById("generate").addEventListener("click",function genera(){
-passwordLength();
-//generatePassword();
+document.getElementById("generate").addEventListener("click",function (){
+  generatePassword();
+
 });
 
+var chooseLength;
+var passwordLowercase;
+var passwordUppercase;
+var passwordNumber;
+var passwordSpecial;
+var allChars;
 // get password length
-var passwordLength=function () {
+var generatePassword=function () {
   var chooseLength = window.prompt("Please enter a number between \"8\" to \"128\" for the chooseLength of your password. You can include 8 or 128.");
   //convert answer to actual number
   chooseLength = parseInt(chooseLength);
-    if(chooseLength >=8 && chooseLength <= 128){
-      window.alert("Great! Your password will have " + chooseLength +" characters in chooseLength.")
-     console.log(chooseLength);
-      passwordInclude ();
-    } 
-    // validate answer
-    else{
-      window.alert("You did not enter a valid number. Try again. ");
-    }
-}
-
+ // validate answer
+  if (!chooseLength ||chooseLength <8 || chooseLength >128){ 
+    window.alert("You did not enter a valid number. Try again.");
+  } else{
+var passwordInclude =function (){
 // ask if user wants to include lowercase letter
-var passwordInclude = function () {
   var passwordLowercase = window.confirm("Would you like include lowercase letter in your password? Enter Ok for Yes and Cancel for No.");
-  //validate prompt answer
+
   let allChars ='';
   if(passwordLowercase){
     allChars += 'abcdefghijklmnopqrstuvwxyz';
     console.log("lowercase: " + allChars);
     console.log(allChars.length);
   }
-  var passwordUppercase = window.confirm("Would you like include UPPERCASE letter in your password? Enter Ok for Yes and Cancel for No.");
-  //validate prompt answer
+var passwordUppercase = window.confirm("Would you like include UPPERCASE letter in your password? Enter Ok for Yes and Cancel for No.");
+ 
   if(passwordUppercase){
     allChars += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     console.log("uppercase: " + allChars);
     console.log(allChars.length);
   }
-  var passwordNumber= window.confirm("Would you like include numbers in your password? Enter Ok for Yes and Cancel for No.");
+var passwordNumber= window.confirm("Would you like include numbers in your password? Enter Ok for Yes and Cancel for No.");
   if(passwordNumber) {
     allChars +='1234567890';
     console.log("number: " + allChars);
@@ -49,7 +48,7 @@ var passwordInclude = function () {
     allChars += "'!@#$%^&*()_+-=[]{}|;,./'";
     console.log("special: " + allChars);
     console.log(allChars.length);
-  }
+  }};
 
   if(!passwordLowercase && !passwordUppercase && !passwordNumber && !passwordSpecial){
     window.alert("You need to choose at least one type of character to include. Please try again. ")
@@ -57,20 +56,21 @@ var passwordInclude = function () {
     passwordInclude();
   }else{
   window.alert("generating password");
-  
 };
 
-var generatePassword =function(){
-
- let genPassword ="";
- pwLength = parseInt(chooseLength);
- console.log (genPassword);
- console.log(pwLength);
- for (i =0 ; i<length; i++)
- genPassword += allChars(Math.floor(Math.random()* allChars.length));
-return genPassword;
+var genPassword ="";
+ for (var i =0 ; i < chooseLength; i++){
+ includedChars = allChars[Math.floor(Math.random()* allChars.length)];
+ genPassword.push(includedChars);
 };
+var password=genPassword.join("");
+UseInput(password);
+return password;
+}
 
+function UserInput(password){
+  document.getElementById("#password").textContent = password;
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
